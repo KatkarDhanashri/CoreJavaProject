a213d0d0210467acd18fa64af932878b8aea3e4b
Dhanashri katkar <katkardhanashri92@gmail.com>
2023-08-17 21:11:40 +0530

    Array&Linked list

---
+++ Array&LinkedList/src/com/java/array_linkedlist/LinkedListDemo.java
@@ -0,0 +1,40 @@
+package com.java.array_linkedlist;
+
+import java.util.LinkedList;
+
+public class LinkedListDemo {
+public static void main(String[] args) {
+	LinkedList<String>cars=new LinkedList<>();
+	cars.add("BMW");
+	cars.add("Innova");
+	cars.add("Ertiga");
+	cars.add("Aoudi");
+	cars.add("Tata");
+	cars.add("Maruti");
+	System.out.println(cars);
+	
+	//add first
+	cars.addFirst("Suzuki");
+	System.out.println(cars);
+	
+	//add last
+		cars.addLast("Suzuki");
+		System.out.println(cars);
+	
+		//remove first
+		cars.removeFirst();
+		System.out.println(cars);
+		
+		//remove last
+		cars.removeLast();
+		System.out.println(cars);
+		
+		//get first
+	    System.out.println(cars.getFirst());
+				
+		//get last
+		System.out.println(cars.getLast());
+		
+				
+}
+}
